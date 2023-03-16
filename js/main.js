7b88d530fdc94595d3bd504f5a7939110d6123cb
Milenko Nikolić <milenko.nikolic@elephantsolutions.net>
2023-03-16 13:18:17 +0100

    Home page tabs

--- js/main.js
+++ js/main.js
@@ -35,47 +35,4 @@ dropdownBtn1.addEventListener('click', function () {
 
  });
 
-//list of categories
-const sapCon = document.getElementById('sapconsulting');
-const independent = document.getElementById('independent');
-const sapClients = document.getElementById('sap-clients');
-
-//block of text for each category
-const sapConBlock = document.getElementById('sap-consulting-block');
-const independentBlock = document.getElementById('independent-block');
-const sapClientsBlock = document.getElementById('sap-clients-block');
-
-
-sapCon.addEventListener('click', (e) => {
-  sapClients.classList.remove('bg-brightBlue');
-  sapCon.classList.add('bg-brightBlue');
-  independent.classList.remove('bg-brightBlue');
-
-  sapConBlock.classList.remove('md:hidden');
-  sapClientsBlock.classList.add('flex');
-  independentBlock.classList.add('hidden');
-  sapClientsBlock.classList.add('hidden');
-});
-
-independent.addEventListener('click', () => {
-  sapClients.classList.remove('bg-brightBlue');
-  sapCon.classList.remove('bg-brightBlue');
-  independent.classList.add('bg-brightBlue');
-
-  independentBlock.classList.remove('md:hidden');
-  independentBlock.classList.add('flex');
-  sapConBlock.classList.add('hidden');
-  sapClientsBlock.classList.add('hidden');
-});
-
-sapClients.addEventListener('click', () => {
-  sapClients.classList.add('bg-brightBlue');
-  sapCon.classList.remove('bg-brightBlue');
-  independent.classList.remove('bg-brightBlue');
-
-  sapClientsBlock.classList.remove('md:hidden');
-  sapClientsBlock.classList.add('flex');
-  sapConBlock.classList.add('hidden');
-  independentBlock.classList.add('hidden');
-});
 
